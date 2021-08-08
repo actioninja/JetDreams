651795489e3c96a9bbd867107765d3932536fa6c
GitHub Action <action@github.com>
2021-08-08 12:57:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/actioninja/jetdreams/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/actioninja/jetdreams/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.actioninja.jetdreams.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.actioninja.jetdreams.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/actioninja/jetdreams/services/MyProjectService.kt
+++ src/main/kotlin/com/github/actioninja/jetdreams/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.actioninja.jetdreams.services
 
+import com.github.actioninja.jetdreams.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
